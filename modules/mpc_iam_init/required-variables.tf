142d9c44299e2332fbe773102ecef9e549d49de8
Craig Rueda <craig@craigrueda.com>
2023-10-18 10:54:40 -0700

    Adding external_id for STS role assumption (#7)

--- modules/mpc_iam_init/required-variables.tf
+++ modules/mpc_iam_init/required-variables.tf
@@ -13,3 +13,8 @@ variable "preset_target_env_account_id" {
   description = "preset_target_env_account_id aws account number"
   type        = string
 }
+
+variable "preset_sts_external_id" {
+  description = "The external ID provided by preset for role assumption"
+  type = string
+}
